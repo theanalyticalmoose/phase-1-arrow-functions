75d26c8cc903eeb01cf293a6825ad4f0a352574b
Rachael <rachael.brady@theanalyticalmoose.com>
2023-09-04 18:31:19 -0700

    complete

---
+++ index.js
@@ -0,0 +1,12 @@
+// divide Function Expression
+const divide = function(a, b) {
+    return a / b;
+  }
+  
+  // square Arrow Function
+  const square = x => x * x;
+  
+  // add Arrow Function
+  const add = (a, b) => a + b;
+  
+  
